7556ebf045eac04b2da59f9ecefd454097c4b5ca
David Smith <dbsmith.dbs83@gmail.com>
2016-08-15 16:47:37 -0700

    basics

---
+++ src/client.py
@@ -0,0 +1,32 @@
+import socket
+import sys
+
+
+
+
+def send_msg(msg):
+    infos = socket.getaddrinfo('127.0.0.1', 5000)
+    stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
+    client = socket.socket(*stream_info[:3])
+    client.connect(stream_info[-1])
+    client.sendall(msg.encode('utf-8'))
+    buffer_length = 8
+    reply_complete = False
+    while not reply_complete:
+        part = client.recv(buffer_length)
+        print(part.decode('utf-8'))
+        if len(part) < buffer_length:
+            message_complete = True
+    client.close()
+    print(part)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(u'usage: python3 client.py <message to send>')
+        sys.exit(1)
+    else:
+        send_msg(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
---
+++ src/server.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+address = ('127.0.0.1', 5000)
+server.bind(address)
+
+server.listen(1)
+conn, addr = server.accept()
+
+buffer_length = 8
+message_complete = False
+
+while not message_complete:
+    part = conn.recv(buffer_length)
+    print(part.decode('utf-8'))
+    if len(part) < buffer_length:
+        message_complete = True
+print(part)
+
+conn.sendall(part.encode('utf-8'))
+conn.close()
+server.close()
