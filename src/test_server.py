0e9d117e207cbad05bba73382caa071f8228792e
David Smith <dbsmith.dbs83@gmail.com>
2016-08-17 19:35:31 -0700

    starting tests, split headers, etc.

--- src/test_server.py
+++ src/test_server.py
@@ -3,12 +3,10 @@ import pytest
 import client
 
 MSG_TABLE = [
-    ('morethan8', 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nmorethan8'),
-    ('eight888', 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\neight888'),
-    ('less8', 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nless8'),
-    ('éyçño', 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\néyçño'),
-    ('', 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n'),
+    ('GET localhost:5000 HTTP/1.1\r\nHost: localhost:5000\r\n\r\n'),
+
 ]
+
 
 
 @pytest.mark.parametrize('msg, result', MSG_TABLE)
